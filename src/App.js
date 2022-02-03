62cb8cfc3752d3aa784f227b8fcb2d29947ad31d
Paulo Castro <paulo.castro172004@gmail.com>
2022-02-03 19:16:48 -0300

    Page Home Complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import RoutesApp from "./routes";
 
 function App(){
     return(
         <div className="app">
-            <h1>Sujeito Link</h1>
+            <RoutesApp />
         </div>
     );
 }
---
+++ src/pages/links/index.js
@@ -0,0 +1,8 @@
+
+function Links(){
+    return(
+        <h1>Page Links</h1>
+    );
+}
+
+export default Links;
